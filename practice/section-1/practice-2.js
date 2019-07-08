59495c9497f636d76210bba9376b572ac3108b98
EElephant <39041247+EElephant@users.noreply.github.com>
2019-07-08 11:54:41 +0800

    js task

--- practice/section-1/practice-2.js
+++ practice/section-1/practice-2.js
@@ -1,5 +1,14 @@
 'use strict';
 
 function collectSameElements(collectionA, collectionB) {
-  return '实现练习要求，并改写该行代码。';
+
+  const collectionC = [];
+  for(var i=0;i<9;i++){
+      for(var j=0;j<4;j++){
+        if(collectionA[i]==(collectionB[0][j])){
+          collectionC.push(collectionA[i])
+        }
+      }
+    }
+  return collectionC;
 }
